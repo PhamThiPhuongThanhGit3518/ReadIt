40492d13dacea86974a501c275bc59d2d1c7ba9e
phamt <phamthiphuongthanh36@gmail.com>
2026-01-27 08:51:06 +0700

    feat: add text button Demo Story Detail to demo UI

--- lib/views/reader/home_screen.dart
+++ lib/views/reader/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -17,33 +18,45 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       body: SafeArea(
           child: Padding(
-              padding: EdgeInsets.all(12),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                TextField(
-                  controller: searchController,
-                  onChanged: (value) => setState(() => searchText = value),
-                  decoration: InputDecoration(
-                    hintText: 'Search story or author',
-                    prefixIcon: Icon(Icons.search),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(12)
-                    )
+            padding: EdgeInsets.all(12),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TextField(
+                    controller: searchController,
+                    onChanged: (value) => setState(() => searchText = value),
+                    decoration: InputDecoration(
+                        hintText: 'Search story or author',
+                        prefixIcon: Icon(Icons.search),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12)
+                        )
+                    ),
                   ),
-                ),
-                const SizedBox(height: 24),
-                Text(
-                  'New Updates',
-                  style: Theme.of(context).textTheme.displayLarge?.copyWith(fontSize: 24),
-                ),
-                const SizedBox(height: 24,),
-                Text(
-                  'Most Popular',
-                  style: Theme.of(context).textTheme.displayLarge?.copyWith(fontSize: 24),
-                ),
-              ],
-            ),
+                  const SizedBox(height: 24),
+                  Text(
+                    'New Updates',
+                    style: Theme.of(context).textTheme.displayLarge?.copyWith(fontSize: 24),
+                  ),
+                  const SizedBox(height: 24,),
+                  Text(
+                    'Most Popular',
+                    style: Theme.of(context).textTheme.displayLarge?.copyWith(fontSize: 24),
+                  ),
+                  const SizedBox(height: 24,),
+                  GestureDetector(
+                    onTap: () {
+                      context.push('/story_detail');
+                    },
+                    child: Text(
+                      'Demo Story Details',
+                      style: Theme.of(context).textTheme.displayMedium?.copyWith(color: Theme.of(context).colorScheme.onSurface),
+                    ),
+                  )
+                ],
+              ),
+            )
           )
       ),
     );
