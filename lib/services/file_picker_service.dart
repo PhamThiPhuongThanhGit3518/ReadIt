3dde45e07eafb57bc954f6718c6976fcf9b48115
phamt <phamthiphuongthanh36@gmail.com>
2026-01-27 08:52:32 +0700

    feat: add file_picker_service.dart

---
+++ lib/services/file_picker_service.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:file_picker/file_picker.dart';
+
+class FilePickerService {
+  final ImagePicker _picker = ImagePicker();
+
+  Future<String?> pickImageFromGallery() async {
+    final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+    return image?.path;
+  }
+
+  Future<String?> pickDocument() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+      type: FileType.custom,
+      allowedExtensions: ['pdf'],
+    );
+    return result?.files.single.path;
+  }
+}
+
+final filePickerServiceProvider = Provider((ref) => FilePickerService());
