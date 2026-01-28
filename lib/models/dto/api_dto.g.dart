17b975d738dc9b9e28a10a662c94e5ec0f3c6969
phamt <phamthiphuongthanh36@gmail.com>
2026-01-28 10:11:33 +0700

    chore: update auto-generated files from build_runner

---
+++ lib/models/dto/api_dto.g.dart
@@ -0,0 +1,279 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'api_dto.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+RegisterRequest _$RegisterRequestFromJson(Map<String, dynamic> json) =>
+    RegisterRequest(
+      username: json['username'] as String,
+      email: json['email'] as String,
+      password: json['password'] as String,
+      role: json['role'] as String?,
+    );
+
+Map<String, dynamic> _$RegisterRequestToJson(RegisterRequest instance) =>
+    <String, dynamic>{
+      'username': instance.username,
+      'email': instance.email,
+      'password': instance.password,
+      'role': instance.role,
+    };
+
+LoginRequest _$LoginRequestFromJson(Map<String, dynamic> json) => LoginRequest(
+  email: json['email'] as String,
+  password: json['password'] as String,
+);
+
+Map<String, dynamic> _$LoginRequestToJson(LoginRequest instance) =>
+    <String, dynamic>{'email': instance.email, 'password': instance.password};
+
+AuthResponse _$AuthResponseFromJson(Map<String, dynamic> json) => AuthResponse(
+  token: json['token'] as String?,
+  user: json['user'] == null
+      ? null
+      : UserDto.fromJson(json['user'] as Map<String, dynamic>),
+);
+
+Map<String, dynamic> _$AuthResponseToJson(AuthResponse instance) =>
+    <String, dynamic>{'token': instance.token, 'user': instance.user};
+
+UserDto _$UserDtoFromJson(Map<String, dynamic> json) => UserDto(
+  id: (json['id'] as num?)?.toInt(),
+  username: json['username'] as String?,
+  email: json['email'] as String?,
+  role: json['role'] as String?,
+  createdAt: json['createdAt'] == null
+      ? null
+      : DateTime.parse(json['createdAt'] as String),
+);
+
+Map<String, dynamic> _$UserDtoToJson(UserDto instance) => <String, dynamic>{
+  'id': instance.id,
+  'username': instance.username,
+  'email': instance.email,
+  'role': instance.role,
+  'createdAt': instance.createdAt?.toIso8601String(),
+};
+
+UpdateProfileRequest _$UpdateProfileRequestFromJson(
+  Map<String, dynamic> json,
+) => UpdateProfileRequest(
+  username: json['username'] as String?,
+  email: json['email'] as String?,
+);
+
+Map<String, dynamic> _$UpdateProfileRequestToJson(
+  UpdateProfileRequest instance,
+) => <String, dynamic>{'username': instance.username, 'email': instance.email};
+
+CreateStoryRequest _$CreateStoryRequestFromJson(Map<String, dynamic> json) =>
+    CreateStoryRequest(
+      title: json['title'] as String,
+      description: json['description'] as String,
+    );
+
+Map<String, dynamic> _$CreateStoryRequestToJson(CreateStoryRequest instance) =>
+    <String, dynamic>{
+      'title': instance.title,
+      'description': instance.description,
+    };
+
+StoryResponse _$StoryResponseFromJson(Map<String, dynamic> json) =>
+    StoryResponse(
+      success: json['success'] as bool,
+      data: json['data'] == null
+          ? null
+          : StoryData.fromJson(json['data'] as Map<String, dynamic>),
+    );
+
+Map<String, dynamic> _$StoryResponseToJson(StoryResponse instance) =>
+    <String, dynamic>{'success': instance.success, 'data': instance.data};
+
+StoryData _$StoryDataFromJson(Map<String, dynamic> json) => StoryData(
+  storyId: (json['storyId'] as num).toInt(),
+  posterFilename: json['posterFilename'] as String?,
+  posterLink: json['posterLink'] as String?,
+);
+
+Map<String, dynamic> _$StoryDataToJson(StoryData instance) => <String, dynamic>{
+  'storyId': instance.storyId,
+  'posterFilename': instance.posterFilename,
+  'posterLink': instance.posterLink,
+};
+
+StoryListResponse _$StoryListResponseFromJson(Map<String, dynamic> json) =>
+    StoryListResponse(
+      success: json['success'] as bool,
+      data: (json['data'] as List<dynamic>?)
+          ?.map((e) => StorySummary.fromJson(e as Map<String, dynamic>))
+          .toList(),
+    );
+
+Map<String, dynamic> _$StoryListResponseToJson(StoryListResponse instance) =>
+    <String, dynamic>{'success': instance.success, 'data': instance.data};
+
+StorySummary _$StorySummaryFromJson(Map<String, dynamic> json) => StorySummary(
+  id: (json['id'] as num).toInt(),
+  title: json['title'] as String,
+  viewCount: (json['viewCount'] as num?)?.toInt(),
+  chapterCount: (json['chapterCount'] as num?)?.toInt(),
+  posterLink: json['posterLink'] as String?,
+);
+
+Map<String, dynamic> _$StorySummaryToJson(StorySummary instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'title': instance.title,
+      'viewCount': instance.viewCount,
+      'chapterCount': instance.chapterCount,
+      'posterLink': instance.posterLink,
+    };
+
+StoryInfoResponse _$StoryInfoResponseFromJson(Map<String, dynamic> json) =>
+    StoryInfoResponse(
+      success: json['success'] as bool,
+      data: json['data'] == null
+          ? null
+          : StoryDetail.fromJson(json['data'] as Map<String, dynamic>),
+    );
+
+Map<String, dynamic> _$StoryInfoResponseToJson(StoryInfoResponse instance) =>
+    <String, dynamic>{'success': instance.success, 'data': instance.data};
+
+StoryDetail _$StoryDetailFromJson(Map<String, dynamic> json) => StoryDetail(
+  id: (json['id'] as num).toInt(),
+  title: json['title'] as String,
+  description: json['description'] as String,
+  chapterCount: (json['chapterCount'] as num).toInt(),
+  viewCount: (json['viewCount'] as num).toInt(),
+  posterLink: json['posterLink'] as String?,
+  createdAt: json['createdAt'] == null
+      ? null
+      : DateTime.parse(json['createdAt'] as String),
+);
+
+Map<String, dynamic> _$StoryDetailToJson(StoryDetail instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'title': instance.title,
+      'description': instance.description,
+      'chapterCount': instance.chapterCount,
+      'viewCount': instance.viewCount,
+      'posterLink': instance.posterLink,
+      'createdAt': instance.createdAt?.toIso8601String(),
+    };
+
+ChapterListResponse _$ChapterListResponseFromJson(Map<String, dynamic> json) =>
+    ChapterListResponse(
+      success: json['success'] as bool,
+      data: (json['data'] as List<dynamic>?)
+          ?.map((e) => ChapterSummary.fromJson(e as Map<String, dynamic>))
+          .toList(),
+    );
+
+Map<String, dynamic> _$ChapterListResponseToJson(
+  ChapterListResponse instance,
+) => <String, dynamic>{'success': instance.success, 'data': instance.data};
+
+ChapterSummary _$ChapterSummaryFromJson(Map<String, dynamic> json) =>
+    ChapterSummary(
+      id: (json['id'] as num).toInt(),
+      orderNum: (json['orderNum'] as num).toInt(),
+      title: json['title'] as String,
+    );
+
+Map<String, dynamic> _$ChapterSummaryToJson(ChapterSummary instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'orderNum': instance.orderNum,
+      'title': instance.title,
+    };
+
+ChapterContentResponse _$ChapterContentResponseFromJson(
+  Map<String, dynamic> json,
+) => ChapterContentResponse(
+  success: json['success'] as bool,
+  data: json['data'] == null
+      ? null
+      : ChapterDetail.fromJson(json['data'] as Map<String, dynamic>),
+);
+
+Map<String, dynamic> _$ChapterContentResponseToJson(
+  ChapterContentResponse instance,
+) => <String, dynamic>{'success': instance.success, 'data': instance.data};
+
+ChapterDetail _$ChapterDetailFromJson(Map<String, dynamic> json) =>
+    ChapterDetail(
+      id: (json['id'] as num).toInt(),
+      orderNum: (json['orderNum'] as num).toInt(),
+      title: json['title'] as String,
+      content: json['content'] as String,
+    );
+
+Map<String, dynamic> _$ChapterDetailToJson(ChapterDetail instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'orderNum': instance.orderNum,
+      'title': instance.title,
+      'content': instance.content,
+    };
+
+AdminUsersResponse _$AdminUsersResponseFromJson(Map<String, dynamic> json) =>
+    AdminUsersResponse(
+      data: (json['data'] as List<dynamic>)
+          .map((e) => UserDto.fromJson(e as Map<String, dynamic>))
+          .toList(),
+    );
+
+Map<String, dynamic> _$AdminUsersResponseToJson(AdminUsersResponse instance) =>
+    <String, dynamic>{'data': instance.data};
+
+SetRoleRequest _$SetRoleRequestFromJson(Map<String, dynamic> json) =>
+    SetRoleRequest(newRole: json['newRole'] as String);
+
+Map<String, dynamic> _$SetRoleRequestToJson(SetRoleRequest instance) =>
+    <String, dynamic>{'newRole': instance.newRole};
+
+SetRoleResponse _$SetRoleResponseFromJson(Map<String, dynamic> json) =>
+    SetRoleResponse(
+      id: (json['id'] as num).toInt(),
+      username: json['username'] as String,
+      role: json['role'] as String,
+    );
+
+Map<String, dynamic> _$SetRoleResponseToJson(SetRoleResponse instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'username': instance.username,
+      'role': instance.role,
+    };
+
+UploadChaptersResponse _$UploadChaptersResponseFromJson(
+  Map<String, dynamic> json,
+) => UploadChaptersResponse(
+  success: json['success'] as bool,
+  message: json['message'] as String,
+  data: (json['data'] as List<dynamic>?)
+      ?.map((e) => UploadedChapter.fromJson(e as Map<String, dynamic>))
+      .toList(),
+);
+
+Map<String, dynamic> _$UploadChaptersResponseToJson(
+  UploadChaptersResponse instance,
+) => <String, dynamic>{
+  'success': instance.success,
+  'message': instance.message,
+  'data': instance.data,
+};
+
+UploadedChapter _$UploadedChapterFromJson(Map<String, dynamic> json) =>
+    UploadedChapter(
+      chapterId: (json['chapterId'] as num).toInt(),
+      title: json['title'] as String,
+    );
+
+Map<String, dynamic> _$UploadedChapterToJson(UploadedChapter instance) =>
+    <String, dynamic>{'chapterId': instance.chapterId, 'title': instance.title};
---
+++ lib/services/api/api_service.g.dart
@@ -0,0 +1,463 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'api_service.dart';
+
+// dart format off
+
+// **************************************************************************
+// RetrofitGenerator
+// **************************************************************************
+
+// ignore_for_file: unnecessary_brace_in_string_interps,no_leading_underscores_for_local_identifiers,unused_element,unnecessary_string_interpolations,unused_element_parameter,avoid_unused_constructor_parameters,unreachable_from_main
+
+class _ApiService implements ApiService {
+  _ApiService(this._dio, {this.baseUrl, this.errorLogger});
+
+  final Dio _dio;
+
+  String? baseUrl;
+
+  final ParseErrorLogger? errorLogger;
+
+  @override
+  Future<UserDto> register(RegisterRequest request) async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    final _data = <String, dynamic>{};
+    _data.addAll(request.toJson());
+    final _options = _setStreamType<UserDto>(
+      Options(method: 'POST', headers: _headers, extra: _extra)
+          .compose(
+            _dio.options,
+            '/api/auth/register',
+            queryParameters: queryParameters,
+            data: _data,
+          )
+          .copyWith(baseUrl: _combineBaseUrls(_dio.options.baseUrl, baseUrl)),
+    );
+    final _result = await _dio.fetch<Map<String, dynamic>>(_options);
+    late UserDto _value;
+    try {
+      _value = UserDto.fromJson(_result.data!);
+    } on Object catch (e, s) {
+      errorLogger?.logError(e, s, _options, response: _result);
+      rethrow;
+    }
+    return _value;
+  }
+
+  @override
+  Future<AuthResponse> login(LoginRequest request) async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    final _data = <String, dynamic>{};
+    _data.addAll(request.toJson());
+    final _options = _setStreamType<AuthResponse>(
+      Options(method: 'POST', headers: _headers, extra: _extra)
+          .compose(
+            _dio.options,
+            '/api/auth/login',
+            queryParameters: queryParameters,
+            data: _data,
+          )
+          .copyWith(baseUrl: _combineBaseUrls(_dio.options.baseUrl, baseUrl)),
+    );
+    final _result = await _dio.fetch<Map<String, dynamic>>(_options);
+    late AuthResponse _value;
+    try {
+      _value = AuthResponse.fromJson(_result.data!);
+    } on Object catch (e, s) {
+      errorLogger?.logError(e, s, _options, response: _result);
+      rethrow;
+    }
+    return _value;
+  }
+
+  @override
+  Future<UserDto> getCurrentUser() async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    const Map<String, dynamic>? _data = null;
+    final _options = _setStreamType<UserDto>(
+      Options(method: 'GET', headers: _headers, extra: _extra)
+          .compose(
+            _dio.options,
+            '/api/auth/me',
+            queryParameters: queryParameters,
+            data: _data,
+          )
+          .copyWith(baseUrl: _combineBaseUrls(_dio.options.baseUrl, baseUrl)),
+    );
+    final _result = await _dio.fetch<Map<String, dynamic>>(_options);
+    late UserDto _value;
+    try {
+      _value = UserDto.fromJson(_result.data!);
+    } on Object catch (e, s) {
+      errorLogger?.logError(e, s, _options, response: _result);
+      rethrow;
+    }
+    return _value;
+  }
+
+  @override
+  Future<UserDto> getProfile() async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    const Map<String, dynamic>? _data = null;
+    final _options = _setStreamType<UserDto>(
+      Options(method: 'GET', headers: _headers, extra: _extra)
+          .compose(
+            _dio.options,
+            '/api/users/profile',
+            queryParameters: queryParameters,
+            data: _data,
+          )
+          .copyWith(baseUrl: _combineBaseUrls(_dio.options.baseUrl, baseUrl)),
+    );
+    final _result = await _dio.fetch<Map<String, dynamic>>(_options);
+    late UserDto _value;
+    try {
+      _value = UserDto.fromJson(_result.data!);
+    } on Object catch (e, s) {
+      errorLogger?.logError(e, s, _options, response: _result);
+      rethrow;
+    }
+    return _value;
+  }
+
+  @override
+  Future<UserDto> updateProfile(UpdateProfileRequest request) async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    final _data = <String, dynamic>{};
+    _data.addAll(request.toJson());
+    final _options = _setStreamType<UserDto>(
+      Options(method: 'PUT', headers: _headers, extra: _extra)
+          .compose(
+            _dio.options,
+            '/api/users/profile',
+            queryParameters: queryParameters,
+            data: _data,
+          )
+          .copyWith(baseUrl: _combineBaseUrls(_dio.options.baseUrl, baseUrl)),
+    );
+    final _result = await _dio.fetch<Map<String, dynamic>>(_options);
+    late UserDto _value;
+    try {
+      _value = UserDto.fromJson(_result.data!);
+    } on Object catch (e, s) {
+      errorLogger?.logError(e, s, _options, response: _result);
+      rethrow;
+    }
+    return _value;
+  }
+
+  @override
+  Future<AdminUsersResponse> getAllUsers() async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    const Map<String, dynamic>? _data = null;
+    final _options = _setStreamType<AdminUsersResponse>(
+      Options(method: 'GET', headers: _headers, extra: _extra)
+          .compose(
+            _dio.options,
+            '/api/users',
+            queryParameters: queryParameters,
+            data: _data,
+          )
+          .copyWith(baseUrl: _combineBaseUrls(_dio.options.baseUrl, baseUrl)),
+    );
+    final _result = await _dio.fetch<Map<String, dynamic>>(_options);
+    late AdminUsersResponse _value;
+    try {
+      _value = AdminUsersResponse.fromJson(_result.data!);
+    } on Object catch (e, s) {
+      errorLogger?.logError(e, s, _options, response: _result);
+      rethrow;
+    }
+    return _value;
+  }
+
+  @override
+  Future<SetRoleResponse> setUserRole(
+    int userId,
+    SetRoleRequest request,
+  ) async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    final _data = <String, dynamic>{};
+    _data.addAll(request.toJson());
+    final _options = _setStreamType<SetRoleResponse>(
+      Options(method: 'PATCH', headers: _headers, extra: _extra)
+          .compose(
+            _dio.options,
+            '/api/users/${userId}/role',
+            queryParameters: queryParameters,
+            data: _data,
+          )
+          .copyWith(baseUrl: _combineBaseUrls(_dio.options.baseUrl, baseUrl)),
+    );
+    final _result = await _dio.fetch<Map<String, dynamic>>(_options);
+    late SetRoleResponse _value;
+    try {
+      _value = SetRoleResponse.fromJson(_result.data!);
+    } on Object catch (e, s) {
+      errorLogger?.logError(e, s, _options, response: _result);
+      rethrow;
+    }
+    return _value;
+  }
+
+  @override
+  Future<StoryResponse> createStory(
+    String title,
+    String description,
+    MultipartFile? posterFile,
+  ) async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    queryParameters.removeWhere((k, v) => v == null);
+    final _headers = <String, dynamic>{};
+    final _data = FormData();
+    _data.fields.add(MapEntry('title', title));
+    _data.fields.add(MapEntry('description', description));
+    if (posterFile != null) {
+      _data.files.add(MapEntry('poster', posterFile));
+    }
+    final _options = _setStreamType<StoryResponse>(
+      Options(
+            method: 'POST',
+            headers: _headers,
+            extra: _extra,
+            contentType: 'multipart/form-data',
+          )
+          .compose(
+            _dio.options,
+            '/api/stories',
+            queryParameters: queryParameters,
+            data: _data,
+          )
+          .copyWith(baseUrl: _combineBaseUrls(_dio.options.baseUrl, baseUrl)),
+    );
+    final _result = await _dio.fetch<Map<String, dynamic>>(_options);
+    late StoryResponse _value;
+    try {
+      _value = StoryResponse.fromJson(_result.data!);
+    } on Object catch (e, s) {
+      errorLogger?.logError(e, s, _options, response: _result);
+      rethrow;
+    }
+    return _value;
+  }
+
+  @override
+  Future<StoryInfoResponse> getStoryInfo(int storyId) async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    const Map<String, dynamic>? _data = null;
+    final _options = _setStreamType<StoryInfoResponse>(
+      Options(method: 'GET', headers: _headers, extra: _extra)
+          .compose(
+            _dio.options,
+            '/api/stories/${storyId}/info',
+            queryParameters: queryParameters,
+            data: _data,
+          )
+          .copyWith(baseUrl: _combineBaseUrls(_dio.options.baseUrl, baseUrl)),
+    );
+    final _result = await _dio.fetch<Map<String, dynamic>>(_options);
+    late StoryInfoResponse _value;
+    try {
+      _value = StoryInfoResponse.fromJson(_result.data!);
+    } on Object catch (e, s) {
+      errorLogger?.logError(e, s, _options, response: _result);
+      rethrow;
+    }
+    return _value;
+  }
+
+  @override
+  Future<StoryListResponse> searchStories(String keyword) async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{r'search': keyword};
+    final _headers = <String, dynamic>{};
+    const Map<String, dynamic>? _data = null;
+    final _options = _setStreamType<StoryListResponse>(
+      Options(method: 'GET', headers: _headers, extra: _extra)
+          .compose(
+            _dio.options,
+            '/api/stories',
+            queryParameters: queryParameters,
+            data: _data,
+          )
+          .copyWith(baseUrl: _combineBaseUrls(_dio.options.baseUrl, baseUrl)),
+    );
+    final _result = await _dio.fetch<Map<String, dynamic>>(_options);
+    late StoryListResponse _value;
+    try {
+      _value = StoryListResponse.fromJson(_result.data!);
+    } on Object catch (e, s) {
+      errorLogger?.logError(e, s, _options, response: _result);
+      rethrow;
+    }
+    return _value;
+  }
+
+  @override
+  Future<UploadChaptersResponse> uploadChapters(
+    int storyId,
+    List<MultipartFile> chapterFiles,
+  ) async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    final _data = FormData();
+    _data.fields.add(MapEntry('storyId', storyId.toString()));
+    _data.files.addAll(chapterFiles.map((i) => MapEntry('chapters', i)));
+    final _options = _setStreamType<UploadChaptersResponse>(
+      Options(
+            method: 'POST',
+            headers: _headers,
+            extra: _extra,
+            contentType: 'multipart/form-data',
+          )
+          .compose(
+            _dio.options,
+            '/api/stories/upload-chapters',
+            queryParameters: queryParameters,
+            data: _data,
+          )
+          .copyWith(baseUrl: _combineBaseUrls(_dio.options.baseUrl, baseUrl)),
+    );
+    final _result = await _dio.fetch<Map<String, dynamic>>(_options);
+    late UploadChaptersResponse _value;
+    try {
+      _value = UploadChaptersResponse.fromJson(_result.data!);
+    } on Object catch (e, s) {
+      errorLogger?.logError(e, s, _options, response: _result);
+      rethrow;
+    }
+    return _value;
+  }
+
+  @override
+  Future<ChapterListResponse> getChapterList(int storyId) async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    const Map<String, dynamic>? _data = null;
+    final _options = _setStreamType<ChapterListResponse>(
+      Options(method: 'GET', headers: _headers, extra: _extra)
+          .compose(
+            _dio.options,
+            '/api/stories/${storyId}/chapters',
+            queryParameters: queryParameters,
+            data: _data,
+          )
+          .copyWith(baseUrl: _combineBaseUrls(_dio.options.baseUrl, baseUrl)),
+    );
+    final _result = await _dio.fetch<Map<String, dynamic>>(_options);
+    late ChapterListResponse _value;
+    try {
+      _value = ChapterListResponse.fromJson(_result.data!);
+    } on Object catch (e, s) {
+      errorLogger?.logError(e, s, _options, response: _result);
+      rethrow;
+    }
+    return _value;
+  }
+
+  @override
+  Future<ChapterContentResponse> getChapterContent(int chapterId) async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    const Map<String, dynamic>? _data = null;
+    final _options = _setStreamType<ChapterContentResponse>(
+      Options(method: 'GET', headers: _headers, extra: _extra)
+          .compose(
+            _dio.options,
+            '/api/stories/chapters/${chapterId}',
+            queryParameters: queryParameters,
+            data: _data,
+          )
+          .copyWith(baseUrl: _combineBaseUrls(_dio.options.baseUrl, baseUrl)),
+    );
+    final _result = await _dio.fetch<Map<String, dynamic>>(_options);
+    late ChapterContentResponse _value;
+    try {
+      _value = ChapterContentResponse.fromJson(_result.data!);
+    } on Object catch (e, s) {
+      errorLogger?.logError(e, s, _options, response: _result);
+      rethrow;
+    }
+    return _value;
+  }
+
+  @override
+  Future<ChapterContentResponse> getChapterByNumber(
+    int storyId,
+    int orderNum,
+  ) async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    const Map<String, dynamic>? _data = null;
+    final _options = _setStreamType<ChapterContentResponse>(
+      Options(method: 'GET', headers: _headers, extra: _extra)
+          .compose(
+            _dio.options,
+            '/api/stories/${storyId}/chapters/${orderNum}',
+            queryParameters: queryParameters,
+            data: _data,
+          )
+          .copyWith(baseUrl: _combineBaseUrls(_dio.options.baseUrl, baseUrl)),
+    );
+    final _result = await _dio.fetch<Map<String, dynamic>>(_options);
+    late ChapterContentResponse _value;
+    try {
+      _value = ChapterContentResponse.fromJson(_result.data!);
+    } on Object catch (e, s) {
+      errorLogger?.logError(e, s, _options, response: _result);
+      rethrow;
+    }
+    return _value;
+  }
+
+  RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
+    if (T != dynamic &&
+        !(requestOptions.responseType == ResponseType.bytes ||
+            requestOptions.responseType == ResponseType.stream)) {
+      if (T == String) {
+        requestOptions.responseType = ResponseType.plain;
+      } else {
+        requestOptions.responseType = ResponseType.json;
+      }
+    }
+    return requestOptions;
+  }
+
+  String _combineBaseUrls(String dioBaseUrl, String? baseUrl) {
+    if (baseUrl == null || baseUrl.trim().isEmpty) {
+      return dioBaseUrl;
+    }
+
+    final url = Uri.parse(baseUrl);
+
+    if (url.isAbsolute) {
+      return url.toString();
+    }
+
+    return Uri.parse(dioBaseUrl).resolveUri(url).toString();
+  }
+}
+
+// dart format on
