7e50793bb52d7f57c685cbafd70616ec6a14e876
phamt <phamthiphuongthanh36@gmail.com>
2026-01-28 21:50:08 +0700

    feat: add func getNewStories, getPopularStories, incrementView

--- lib/repositories/story_repository.dart
+++ lib/repositories/story_repository.dart
@@ -1,4 +1,5 @@
 // repositories/story_repository.dart
+import 'dart:convert';
 import 'dart:io';
 import 'package:dio/dio.dart';
 import '../models/dto/api_dto.dart';
@@ -10,6 +11,16 @@ class StoryRepository {
 
   Future<List<StorySummary>> getStories({String? query}) async {
     final response = await apiService.searchStories(query ?? "");
+    return response.data ?? [];
+  }
+
+  Future<List<StorySummary>> getNewStories() async {
+    final response = await apiService.getNewStories();
+    return response.data ?? [];
+  }
+
+  Future<List<StorySummary>> getPopularStories() async {
+    final response = await apiService.getPopularStories();
     return response.data ?? [];
   }
 
@@ -33,6 +44,10 @@ class StoryRepository {
     return apiService.createStory(title, description, multipartFile);
   }
 
+  Future<CommonResponse> incrementView(int storyId) async {
+    return apiService.incrementView(storyId);
+  }
+
   Future<List<ChapterSummary>> getChapters(int storyId) async {
     final response = await apiService.getChapterList(storyId);
     return response.data ?? [];
@@ -53,13 +68,18 @@ class StoryRepository {
     required String content,
     required int orderNum,
   }) async {
-    final file = await MultipartFile.fromBytes(
-      content.codeUnits,
-      filename: '$orderNum.txt',
+    final contentBytes = utf8.encode(content);
+
+    final file = MultipartFile.fromBytes(
+      contentBytes,
+      filename: 'content.txt',
     );
-    return apiService.uploadChapters(
+
+    return apiService.uploadSingleChapter(
       storyId,
-      [file],
+      title,
+      orderNum,
+      file,
     );
   }
 }
