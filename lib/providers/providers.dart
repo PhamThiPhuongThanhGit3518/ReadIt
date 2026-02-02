3079683a640439324db28e3d807d54f3be357304
phamt <phamthiphuongthanh36@gmail.com>
2026-02-02 10:12:11 +0700

    refactor: delete files not use

--- lib/providers/providers.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:isar_community/isar.dart';
-import '../database/isar_service.dart';
-
-final isarProvider = FutureProvider<Isar>((ref) async {
-  final service = IsarService();
-  return await service.isar;
-});
